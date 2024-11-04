b3ad7b01c69e8056fdd919669431370fb605dcb6
Ashab-Asir <ashabasir101@gmail.com>
2024-11-04 15:30:40 +0600

    implement main page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
-  return <div>Travel Diary</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product></Product>}></Route>
+        <Route path="pricing" element={<Pricing></Pricing>}></Route>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Product() {
+  const x = 23;
+  return <div>Product</div>;
+}
