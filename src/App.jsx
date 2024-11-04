d8a98d98990c8b7b935a8ce52291ef7d9162c371
Ashab-Asir <ashabasir101@gmail.com>
2024-11-04 17:10:25 +0600

    design our app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
         <Route path="product" element={<Product></Product>}></Route>
         <Route path="pricing" element={<Pricing></Pricing>}></Route>
-        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
+        <Route path="app" element={<AppLayout></AppLayout>}></Route>
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { nav } from "./AppNav.module.css";
+export default function AppNav() {
+  return <div className={nav}>app nav</div>;
+}
