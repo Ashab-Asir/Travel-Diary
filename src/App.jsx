0155698a43cf38dce6163d5702d2fe0c0026f926
Ashab-Asir <ashabasir101@gmail.com>
2024-11-04 23:08:01 +0600

    nested route is added

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route index element={<Homepage></Homepage>}></Route>
         <Route path="product" element={<Product></Product>}></Route>
         <Route path="pricing" element={<Pricing></Pricing>}></Route>
         <Route path="login" element={<Login></Login>}></Route>
-        <Route path="app" element={<AppLayout></AppLayout>}></Route>
+        <Route path="app" element={<AppLayout></AppLayout>}>
+          <Route index element={<p>List</p>}></Route>
+          <Route path="cities" element={<p>cities</p>}></Route>
+          <Route path="countries" element={<p>countries</p>}></Route>
+          <Route path="form" element={<p>form</p>}></Route>
+        </Route>
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
     </BrowserRouter>
