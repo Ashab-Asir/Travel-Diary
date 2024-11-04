b5f0b2c1a707aa39ba09b5ec674876e7f223f882
Ashab-Asir <ashabasir101@gmail.com>
2024-11-04 17:23:17 +0600

    designed app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav></AppNav>
+    <div className={styles.app}>
+      <Sidebar></Sidebar>
+      <Map></Map>
     </div>
   );
 }
